62d855f6ababcb60dc26e3b762740d68ddff804f
Maxim-Belyev <125187526+Maxim-Belyev@users.noreply.github.com>
2023-06-04 01:56:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,21 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+ BST<std::string> tree;
+  std::ifstream file(filename);
+  std::string fail;
+  while (!file.eof()) {
+    int f = file.get();
+    if (f >= 65 && f <= 90) {
+      f += 32;
+      fail += f;
+    } else if (f >= 97 && f <= 122) {
+      fail += f;
+    } else {
+      tree.add(fail);
+      fail = "";
+    }
+  }
+  file.close();
+  return tree;
 }
